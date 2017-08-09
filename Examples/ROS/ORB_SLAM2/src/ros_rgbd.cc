b4336ed2916f81a44e0c51f4217b59b4e057853f
xinkedeng <xdeng12@illinois.edu>
2017-08-08 20:20:34 -0500

    pose publisher for RGBD mode - xinke

--- Examples/ROS/ORB_SLAM2/src/ros_rgbd.cc
+++ Examples/ROS/ORB_SLAM2/src/ros_rgbd.cc
@@ -34,16 +34,23 @@
 
 #include"../../../include/System.h"
 
+#include <geometry_msgs/PoseStamped.h>
+#include <tf/transform_broadcaster.h>
+
 using namespace std;
 
 class ImageGrabber
 {
 public:
-    ImageGrabber(ORB_SLAM2::System* pSLAM):mpSLAM(pSLAM){}
+    ImageGrabber(ORB_SLAM2::System* pSLAM, ros::Publisher& posePub):mpSLAM(pSLAM),posePublisher(posePub){}
 
     void GrabRGBD(const sensor_msgs::ImageConstPtr& msgRGB,const sensor_msgs::ImageConstPtr& msgD);
 
+    tf::Transform cvMatToTF ( cv::Mat Tcw );
+
     ORB_SLAM2::System* mpSLAM;
+    geometry_msgs::PoseStamped pose_out_;
+    ros::Publisher posePublisher; 
 };
 
 int main(int argc, char **argv)
@@ -64,20 +71,23 @@ int main(int argc, char **argv)
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
     ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::RGBD,true,NULL,NULL,path_to_map);
 
-    ImageGrabber igb(&SLAM);
-
-    if (is_localization_mode) {
-		cerr << endl << "Activate localization." << endl;
-        SLAM.ActivateLocalizationMode();
-	}   
-
     ros::NodeHandle nh;
 
     message_filters::Subscriber<sensor_msgs::Image> rgb_sub(nh, "/kinect2/qhd/image_mono", 1);
     message_filters::Subscriber<sensor_msgs::Image> depth_sub(nh, "/kinect2/qhd/image_depth_rect", 1);
     typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> sync_pol;
     message_filters::Synchronizer<sync_pol> sync(sync_pol(10), rgb_sub,depth_sub);
+
+    ros::Publisher pose_pub = nh.advertise<geometry_msgs::PoseStamped> ( "/orb/pose_est", 2 );
+
+    ImageGrabber igb(&SLAM, pose_pub);
+
     sync.registerCallback(boost::bind(&ImageGrabber::GrabRGBD,&igb,_1,_2));
+
+    if (is_localization_mode) {
+		cerr << endl << "Activate localization." << endl;
+        SLAM.ActivateLocalizationMode();
+	}   
 
     ros::spin();
 
@@ -123,7 +133,53 @@ void ImageGrabber::GrabRGBD(const sensor_msgs::ImageConstPtr& msgRGB,const senso
         return;
     }
 
-    mpSLAM->TrackRGBD(cv_ptrRGB->image,cv_ptrD->image,cv_ptrRGB->header.stamp.toSec());
+    cv::Mat pose = mpSLAM->TrackRGBD(cv_ptrRGB->image,cv_ptrD->image,cv_ptrRGB->header.stamp.toSec());
+
+    if (pose.empty())
+        return;
+    else{
+        tf::Transform pose_tf = cvMatToTF(pose);
+        tf::Quaternion pose_orientation = pose_tf.getRotation();
+        tf::Vector3 pose_origin = pose_tf.getOrigin();
+
+        pose_out_.header.stamp = cv_ptrRGB->header.stamp;
+        pose_out_.pose.position.x = pose_origin.getX();
+        pose_out_.pose.position.y = pose_origin.getY();
+        pose_out_.pose.position.z = pose_origin.getZ();
+        pose_out_.pose.orientation.x = pose_orientation.getX();
+        pose_out_.pose.orientation.y = pose_orientation.getY();
+        pose_out_.pose.orientation.z = pose_orientation.getZ();
+        pose_out_.pose.orientation.w = pose_orientation.getW();
+
+        posePublisher.publish(pose_out_);
+
+    }
+
+    
+
+}
+
+tf::Transform ImageGrabber::cvMatToTF ( cv::Mat Tcw ) {
+    tf::Transform cam_to_first_keyframe_transform;
+    // invert since Tcw (transform from world to camera)
+    cv::Mat pose = Tcw.inv();
+
+    //Extract transformation from the raw orb SLAM pose
+    tf::Vector3 origin;
+    //tf::Quaternion transform_quat;
+    tf::Matrix3x3 transform_matrix;
+
+    origin.setValue ( pose.at<float> ( 0, 3 ), pose.at<float> ( 1, 3 ), pose.at<float> ( 2, 3 ) );
+
+    transform_matrix.setValue ( pose.at<float> ( 0, 0 ), pose.at<float> ( 0, 1 ), pose.at<float> ( 0, 2 ),
+                                pose.at<float> ( 1, 0 ), pose.at<float> ( 1, 1 ), pose.at<float> ( 1, 2 ),
+                                pose.at<float> ( 2, 0 ), pose.at<float> ( 2, 1 ), pose.at<float> ( 2, 2 ) );
+
+    //transform_matrix.getRotation(transform_quat);
+    cam_to_first_keyframe_transform.setOrigin ( origin );
+    cam_to_first_keyframe_transform.setBasis ( transform_matrix );
+
+    return cam_to_first_keyframe_transform;
 }
 
 
